8294e6513ed9c4f9858df6a505df3bc172791211
ajentwa <ajentwa@gmail.com>
2020-03-27 21:51:49 +0300

    last

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
 	return render(request,'pages/home.html')
 
 def about(request):
     return render(request,'pages/about.html')
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user':user}
+    return render(request,'pages/profile.html',context)
+
+
+
+    
